b464d1c7d1d48d3d46c47e21c0ff99431be81302
Nitin <nitinvupk@gmail.com>
2019-01-07 16:38:05 +0530

    Use date input component in test component

---
+++ lib/test_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular/angular.dart';
+import 'src/date_input_component.dart';
+
+@Component(
+  selector: 'my-app',
+  templateUrl: 'test_component.html',
+  directives: [DateInputComponent]
+)
+
+class TestComponent {
+}
